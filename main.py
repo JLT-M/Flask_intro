16c05ac2ae52aa33f823ded16a943eaf1ea769df
JLT <julita@mokymai.smshostingas.lt>
2022-09-14 16:15:03 +0300

    labas pasauli

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# app = Flask(__name__)
+#
+# @app.route("/<vardas>")
+# def home(vardas):
+#     return f"Labas, {vardas}"
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Čia mano pirmasis Flask puslapis</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
