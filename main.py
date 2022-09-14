c3ed87d369b180bcad7c22698cbbf92ce31c74d9
JLT <julita@mokymai.smshostingas.lt>
2022-09-14 16:53:37 +0300

    login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # app = Flask(__name__)
 #
@@ -11,9 +11,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        vardas = request.form["vardas"]
+        return render_template("greetings.html", vardas=vardas)
+    else:
+        return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
